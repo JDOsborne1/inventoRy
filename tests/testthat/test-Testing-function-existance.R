e376e0073c52483468f4c19f1f028cae031d9702
JDO <atharos@vivaldi.net>
2020-04-14 17:03:02 +0100

    updating existence tests since one function fulfilled two roles and another had been named differently

--- tests/testthat/test-Testing-function-existance.R
+++ tests/testthat/test-Testing-function-existance.R
@@ -1,14 +1,12 @@
 test_that("multiplication works", {
         # Expect the existance of a function to change the pairs in the database, updating the quantity
   expect_true(exists("inv_ChangePairs"))
-        # Expect the existance of a function to import a bulk set of item quantity pairs
-  expect_true(exists("inv_ImportPairsBulk"))
         # Expect the existance of a function to import a set of required quantities in a recipe, along with instructions
   expect_true(exists("inv_CheckRecipe"))
         # Expect the existance of a function to display the inventory
   expect_true(exists("inv_PrintInventory"))
         # Expect the existance of a function to generate a viable reciple from the inventory
-  expect_true(exists("inv_GenRecipe"))
+  expect_true(exists("inv_SelectRecipe"))
         # Expect the existance of a function to produce the yaml compatible form of the input
   expect_true(exists("inv_ConvertToYAML"))
         # Expect the existance of a function to extract the units from the YAML form
