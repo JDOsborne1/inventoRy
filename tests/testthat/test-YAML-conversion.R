b2c04af283e665d23a9fab0b914f63c654bc3a36
JDO <atharos@vivaldi.net>
2020-04-14 16:24:14 +0100

    Making the yaml conversion test more explicit

--- tests/testthat/test-YAML-conversion.R
+++ tests/testthat/test-YAML-conversion.R
@@ -1,6 +1,6 @@
 test_that("conversion works", {
 
-  test_output <- inv_ConvertFromYAML(test_inventory)
+  test_output <- inv_ConvertFromYAML(yaml_list_in = test_inventory)
 
   expect_equal(as.numeric(test_output[["basic"]][["pasta"]]), 500)
 
