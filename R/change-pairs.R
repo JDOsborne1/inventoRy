3e351fb4593f54929e0c08e8b8c583bb03a5f868
JDO <atharos@vivaldi.net>
2020-04-14 16:25:16 +0100

    Adding in pair update function

---
+++ R/change-pairs.R
@@ -0,0 +1,22 @@
+inv_ChangePairs <- function(pair_changes, .inventory = test_inventory, .history = test_history ){
+        parsed_inventory <- inv_ConvertFromYAML(.inventory)
+
+
+
+        for (i in names(pair_changes)){
+                parsed_inventory[["basic"]][[i]] <- parsed_inventory[["basic"]][[i]] + pair_changes[[i]]
+        }
+
+        for(i in names(pair_changes)){
+                new_history <- tibble::tribble(~object, ~quantity, ~unit,  ~time
+                                               , i, as.character(parsed_inventory[["basic"]][[i]]), units::deparse_unit(parsed_inventory[["basic"]][[i]]), lubridate::as_datetime(Sys.time())
+                )
+                .history <- dplyr::bind_rows(.history, new_history)
+        }
+
+        list(
+                "new_inventory" = inv_ConvertToYAML(parsed_inventory)
+                , "history" = .history
+        )
+}
+
