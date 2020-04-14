6db17d1004b6fe0ec4738223ddba09dbe6141167
JDO <atharos@vivaldi.net>
2020-04-14 16:23:05 +0100

    Expanding the example recipes

--- data-raw/test_recipes.R
+++ data-raw/test_recipes.R
@@ -3,9 +3,40 @@
 test_recipes <- list(
         "basic" = list(
                 "tuna pasta bake" = list(
+                        "ingredients" = list(
+
                         "pasta" = list(
-                                "value" = 200
+                                "value" = 400
                                 , "unit" = "g"
+                        )
+                        , "tuna" = list(
+                                "value" = 400
+                                , "unit" = "g"
+                        )
+                        , "cheddar" = list(
+                                "value" = 100
+                                , "unit" = "g"
+                        )
+                        )
+                        , "Process" = list(
+                                "Preheat the oven to 200C Fan / Gas Mark 7"
+                                , "Boil the Kettle and fill a large saucepan"
+                                , "etc add more"
+                        )
+                )
+                , "orange pasta (test obvs)" = list(
+                        "ingredients" = list(
+                                "pasta" = list(
+                                        "value" = 30
+                                        , "unit" = "g"
+                                )
+                                , "oranges" = list(
+                                        "value" = 1
+                                        , "unit" = "count"
+                                        )
+                        )
+                        , "Process" = list(
+                                "etc add more"
                         )
                 )
         )
